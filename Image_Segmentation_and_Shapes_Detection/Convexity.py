77ae80bde564f3634171d6adcdc48885a08ba543
LissetheGarcia <109912046+LissetheGarcia@users.noreply.github.com>
2022-09-19 01:07:31 -0500

    Update and rename p2.py to Convexity.py

--- Image_Segmentation_and_Shapes_Detection/Convexity.py
+++ Image_Segmentation_and_Shapes_Detection/Convexity.py
@@ -1,8 +1,3 @@
-# García Santoyo, Lissethe Alejandra 17110106
-# Sistemas de visión artificial y procesamiento de imágenes
-# Chapter 8:"Detectando formas y segmentando una imagen"
-# Archive: "programa_2_17110106.py"
-
 # Formas convexas
 
 import sys
