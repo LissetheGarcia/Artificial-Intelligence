6a29183a8b7c35366c9816b8c5a9099874e4c4fd
LissetheGarcia <109912046+LissetheGarcia@users.noreply.github.com>
2022-09-19 01:15:03 -0500

    Update and rename p5.py to Background_Subtraction.py

--- Object_Tracking/Background_Subtraction.py
+++ Object_Tracking/Background_Subtraction.py
@@ -1,8 +1,3 @@
-# García Santoyo, Lissethe Alejandra 17110106
-# Sistemas de visión artificial y procesamiento de imágenes
-# Chapter 9:"Seguimiento de objetos"
-# Archive: "programa_5_17110106.py"
-
 # Resta de fondo
 
 import cv2
