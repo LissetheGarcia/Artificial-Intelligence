d420a44449895c68076b9687a96c80864312993e
LissetheGarcia <109912046+LissetheGarcia@users.noreply.github.com>
2022-09-19 00:58:16 -0500

    Update and rename p5.py to Binary_Robust_Independent_Elementary_Features.py

--- Feature_Extraction/Binary_Robust_Independent_Elementary_Features.py
+++ Feature_Extraction/Binary_Robust_Independent_Elementary_Features.py
@@ -1,9 +1,3 @@
-# García Santoyo, Lissethe Alejandra 17110106
-# Sistemas de visión artificial y procesamiento de imágenes
-# Chapter 5:"Extrayendo características de una imagen"
-# Archive: "programa_5_17110106.py"
-
-
 # BRIEF (Binary Robust Independent elemntary Features)
 
 import cv2
