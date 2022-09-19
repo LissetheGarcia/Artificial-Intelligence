bc7165abd85e033c77c9ee56c0171d90014880a0
LissetheGarcia <109912046+LissetheGarcia@users.noreply.github.com>
2022-09-19 01:10:23 -0500

    Update and rename p2.py to Support_Vector_Machines.py

--- Object_Recognition/Support_Vector_Machines.py
+++ Object_Recognition/Support_Vector_Machines.py
@@ -1,8 +1,3 @@
-# García Santoyo, Lissethe Alejandra 17110106
-# Sistemas de visión artificial y procesamiento de imágenes
-# Capítulo 10:"Reconocimiento de objetos"
-# Archivo: "programa_2_17110106.py"
-
 # Soporte de máquinas de vectores
 
 import os
