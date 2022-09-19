52d62035bc30bec8f2180480bbf4ecdfc74253df
LissetheGarcia <109912046+LissetheGarcia@users.noreply.github.com>
2022-09-19 01:17:00 -0500

    Update and rename p1.py to Epipolar_Geometry.py

--- Reconstruction3D_and_Stereo_Vision/Epipolar_Geometry.py
+++ Reconstruction3D_and_Stereo_Vision/Epipolar_Geometry.py
@@ -1,8 +1,3 @@
-# García Santoyo, Lissethe Alejandra 17110106
-# Sistemas de visión artificial y procesamiento de imágenes
-# Capítulo 11:"Estereovisión y reconstrución 3D"
-# Archivo: "programa_1_17110106.py"
-
 # Geometría Epipolar
 
 import argparse
