7cae9197e3ee683cfe5c92a285d0b2dcb608ad06
LissetheGarcia <109912046+LissetheGarcia@users.noreply.github.com>
2022-09-19 01:09:37 -0500

    Update and rename p1.py to Dense_Characteristics_Detection.py

--- Object_Recognition/Dense_Characteristics_Detection.py
+++ Object_Recognition/Dense_Characteristics_Detection.py
@@ -1,8 +1,3 @@
-# García Santoyo, Lissethe Alejandra 17110106
-# Sistemas de visión artificial y procesamiento de imágenes
-# Capítulo 10:"Reconocimiento de objetos"
-# Archivo: "programa_1_17110106.py"
-
 # Detector de características densas
 
 import cv2
