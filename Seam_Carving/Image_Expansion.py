50a6b8b8ae161db85de8348f587f82cfd69a9124
LissetheGarcia <109912046+LissetheGarcia@users.noreply.github.com>
2022-09-19 01:18:51 -0500

    Update and rename p2.py to Image_Expansion.py

--- Seam_Carving/Image_Expansion.py
+++ Seam_Carving/Image_Expansion.py
@@ -1,8 +1,3 @@
-# García Santoyo, Lissethe Alejandra 17110106
-# Sistemas de visión artificial y procesamiento de imágenes
-# Chapter 7:"Cambio del tamaño de una imagen"
-# Archive: "programa_2_17110106.py"
-
 # Ensanchando/expandiendo una imagen
 
 import sys
